75ef57825edc1cb36d96c55aae529c7646e24412
DanielRasho <ray22933@uvg.edu.gt>
2026-06-28 18:32:06 -0600

    feat: add dependencies to flake

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
           libsoup_3
           webkitgtk_4_1
           librsvg
+          gst_all_1.gstreamer
+          gst_all_1.gst-plugins-base
         ];
       in {
         devShells.default = pkgs.mkShell {
@@ -37,10 +39,11 @@
           ]);
 
           shellHook = ''
-            echo "HELLO WORLD"
+            echo "HELLO WORLD!"
             export WEBKIT_DISABLE_DMABUF_RENDERER="1"
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH"
             export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
+            export FONTCONFIG_FILE="${pkgs.fontconfig}/etc/fonts/fonts.conf"
             alias tauri-dev="${nixglPkg}/bin/nixGLIntel pnpm tauri dev"
             echo "Run 'tauri-dev' to start with nixGL wrapping"
           '';
